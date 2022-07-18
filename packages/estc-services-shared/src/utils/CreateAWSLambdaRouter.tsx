b907f24fe8f9c7915ba158c802a7f4fd3ad020ae
Altay Batuhan <altaybatuhanmail@gmail.com>
2022-07-18 13:35:58 -0700

    Add services-shared package - added CreateAWSLambdaRouter

---
+++ packages/estc-services-shared/src/utils/CreateAWSLambdaRouter.tsx
@@ -0,0 +1,13 @@
+import * as trpc from '@trpc/server';
+import { CreateAWSLambdaContextOptions } from '@trpc/server/adapters/aws-lambda';
+import type { APIGatewayProxyEventV2 } from 'aws-lambda';
+
+export const createContext = ({
+    event,
+    context,
+}: CreateAWSLambdaContextOptions<APIGatewayProxyEventV2>) => ({}) // 
+type Context = trpc.inferAsyncReturnType<typeof createContext>;
+
+export const createAWSLambdaRouter = () => {
+    return trpc.router<Context>();
+}
