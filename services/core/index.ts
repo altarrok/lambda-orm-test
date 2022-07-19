54e88922317f291ed0c20bfc3dd69ce56ef90f72
Altay Batuhan <altaybatuhanmail@gmail.com>
2022-07-18 17:12:46 -0700

    abstracted the trpc router away from index

--- services/core/index.ts
+++ services/core/index.ts
@@ -1,8 +1,1 @@
-import { createAWSLambdaRouter } from '@ino-estc/services-shared';
-import { TournamentAppRouter } from '@ino-estc/tournament';
-
-const appRouter = createAWSLambdaRouter()
-    .merge(TournamentAppRouter)
-
-
-export type AppRouter = typeof appRouter;
+export * from './src';
